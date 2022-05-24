a1bde8968c7700bb481b50e7c5af171198fdca28
HugoMeirelles <hugoestevam@gmail.com>
2022-05-24 08:28:49 -0300

    BackEnd .Net curso

---
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProEventos.API.Models;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+
+        public EventoController()
+        {
+
+        }
+
+
+        public IEnumerable<Evento> _evento = new Evento[]{
+            new Evento(){
+                EventoId = 1,
+                Tema = "Angular 11 e .NET 5 com Hugo Meirelles",
+                Local = "Brasilia",
+                Lote = "1º Lote",
+                QtdPessoas = 250,
+                DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
+                ImagemURL = "foto.png"
+            },
+            new Evento(){
+                EventoId = 2,
+                Tema = "FullStack e .NET 5 com Hugo Meirelles",
+                Local = "Brasilia",
+                Lote = "1º Lote",
+                QtdPessoas = 250,
+                DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
+                ImagemURL = "foto.png"                    
+            }
+                
+        };
+
+        [HttpGet]
+        public IEnumerable<Evento> Get()
+        {
+            return _evento;
+        }
+
+        [HttpGet("{id}")]
+        public IEnumerable<Evento> GetById(int id)
+        {
+            return _evento.Where(evento => evento.EventoId == id);
+        }
+
+
+
+    }
+}
---
+++ Back/src/ProEventos.API/Controllers/TemaController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TemaController : ControllerBase
+    {
+
+        public TemaController()
+        {
+           
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "Metodo Get chamado";
+        }
+        
+        [HttpPost]
+        public string Post()
+        {
+            return "Metodo Post chamado";
+        }
+
+        [HttpPut("{id}")]
+        public string Put(int id)
+        {
+            return $"Metodo Put chamado id:{id}";
+        }
+        [HttpDelete("{id}")]
+        public string Delete(int id)
+        {
+            return $"Metodo Delete chamado id: {id}";
+        }
+    }
+}
